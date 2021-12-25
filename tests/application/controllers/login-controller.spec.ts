141dfbd2a59112931efff0279932fb3b20881515
Jonatan Flores <jonatafloress@gmail.com>
2021-12-25 10:44:11 -0300

    ensure LoginController returns 400 if email is null

--- tests/application/controllers/login-controller.spec.ts
+++ tests/application/controllers/login-controller.spec.ts
@@ -24,4 +24,15 @@ describe('LoginController', () => {
       data: new Error('The email field is required')
     })
   })
+
+  test('should return 400 if email is null', async () => {
+    const sut = new LoginController()
+
+    const httpResponse = await sut.handle({ email: null })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The email field is required')
+    })
+  })
 })
