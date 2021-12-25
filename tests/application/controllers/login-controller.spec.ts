86f1d618e866639243e730c344c9514f47cf735d
Jonatan Flores <jonatafloress@gmail.com>
2021-12-25 13:15:36 -0300

    change LoginController tests to mock RequiredStringValidator instead of test in integration

--- tests/application/controllers/login-controller.spec.ts
+++ tests/application/controllers/login-controller.spec.ts
@@ -1,85 +1,58 @@
 import { LoginController } from '@/application/controllers'
 import { UnauthorizedError, ServerError } from '@/application/errors'
 import { AuthenticationError } from '@/domain/entities/errors'
+import { RequiredStringValidator } from '@/application/validation'
+
+import { mocked } from 'ts-jest/utils'
+
+jest.mock('@/application/validation/required-string')
 
 describe('LoginController', () => {
+  let email: string
+  let password: string
+  let token: string
   let authentication: jest.Mock
   let sut: LoginController
 
   beforeAll(() => {
+    email = 'any_email'
+    password = 'any_password'
+    token = 'any_token'
     authentication = jest.fn()
-    authentication.mockResolvedValue({ accessToken: 'any_value' })
+    authentication.mockResolvedValue({ accessToken: token })
   })
 
   beforeEach(() => {
     sut = new LoginController(authentication)
   })
 
-  test('should return 400 if email is empty', async () => {
-    const httpResponse = await sut.handle({ email: '', password: 'any_password' })
+  test('should return 400 if validation fails', async () => {
+    const error = new Error('validation_error')
+    const RequiredStringValidatorSpy = jest.fn().mockImplementationOnce(() => ({
+      validate: jest.fn().mockReturnValueOnce(error)
+    }))
+    mocked(RequiredStringValidator).mockImplementationOnce(RequiredStringValidatorSpy)
 
+    const httpResponse = await sut.handle({ email, password })
+
+    expect(RequiredStringValidator).toHaveBeenCalledWith(email, 'email')
     expect(httpResponse).toEqual({
       statusCode: 400,
-      data: new Error('The email field is required')
-    })
-  })
-
-  test('should return 400 if email is null', async () => {
-    const httpResponse = await sut.handle({ email: null as any, password: 'any_password' })
-
-    expect(httpResponse).toEqual({
-      statusCode: 400,
-      data: new Error('The email field is required')
-    })
-  })
-
-  test('should return 400 if email is undefined', async () => {
-    const httpResponse = await sut.handle({ email: undefined as any, password: 'any_password' })
-
-    expect(httpResponse).toEqual({
-      statusCode: 400,
-      data: new Error('The email field is required')
-    })
-  })
-
-  test('should return 400 if password is empty', async () => {
-    const httpResponse = await sut.handle({ email: 'any@mail.com', password: '' })
-
-    expect(httpResponse).toEqual({
-      statusCode: 400,
-      data: new Error('The password field is required')
-    })
-  })
-
-  test('should return 400 if password is null', async () => {
-    const httpResponse = await sut.handle({ email: 'any@mail.com', password: null as any })
-
-    expect(httpResponse).toEqual({
-      statusCode: 400,
-      data: new Error('The password field is required')
-    })
-  })
-
-  test('should return 400 if password is undefined', async () => {
-    const httpResponse = await sut.handle({ email: 'any@mail.com', password: undefined as any })
-
-    expect(httpResponse).toEqual({
-      statusCode: 400,
-      data: new Error('The password field is required')
+      data: error
     })
   })
 
   test('should call Authentication with correct input', async () => {
-    await sut.handle({ email: 'any@mail.com', password: 'any_password' })
+    await sut.handle({ email, password })
 
-    expect(authentication).toHaveBeenCalledWith({ email: 'any@mail.com', password: 'any_password' })
+    expect(authentication).toHaveBeenCalledWith({ email, password })
     expect(authentication).toHaveBeenCalledTimes(1)
   })
 
   test('should return 401 if authentication fails', async () => {
     authentication.mockRejectedValueOnce(new AuthenticationError())
 
-    const httpResponse = await sut.handle({ email: 'any@mail.com', password: 'any_password' })
+    const httpResponse = await sut.handle({ email, password })
 
     expect(httpResponse).toEqual({
       statusCode: 401,
@@ -91,7 +64,7 @@ describe('LoginController', () => {
     const error = new Error('infra_error')
     authentication.mockRejectedValueOnce(error)
 
-    const httpResponse = await sut.handle({ email: 'any@mail.com', password: 'any_password' })
+    const httpResponse = await sut.handle({ email, password })
 
     expect(httpResponse).toEqual({
       statusCode: 500,
@@ -100,11 +73,11 @@ describe('LoginController', () => {
   })
 
   test('should return 200 if authentication succeeds', async () => {
-    const httpResponse = await sut.handle({ email: 'any@mail.com', password: 'any_password' })
+    const httpResponse = await sut.handle({ email, password })
 
     expect(httpResponse).toEqual({
       statusCode: 200,
-      data: { accessToken: 'any_value' }
+      data: { accessToken: token }
     })
   })
 })
