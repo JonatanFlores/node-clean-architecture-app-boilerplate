1bc6d0b2c622f4a565292cc7f5447c641f95f0b6
Jonatan Flores <jonatafloress@gmail.com>
2021-12-25 10:42:58 -0300

    ensure LoginController returns 400 if email is empty

---
+++ tests/application/controllers/login-controller.spec.ts
@@ -0,0 +1,27 @@
+
+class LoginController {
+  async handle (httpRequest: any): Promise<HttpResponse> {
+    return {
+      statusCode: 400,
+      data: new Error('The email field is required')
+    }
+  }
+}
+
+type HttpResponse = {
+  statusCode: number
+  data: any
+}
+
+describe('LoginController', () => {
+  test('should return 400 if email is empty', async () => {
+    const sut = new LoginController()
+
+    const httpResponse = await sut.handle({ email: '' })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The email field is required')
+    })
+  })
+})
