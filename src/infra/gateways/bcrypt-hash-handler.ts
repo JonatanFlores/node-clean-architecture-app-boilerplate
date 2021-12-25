5792672f721410c1f1112977119748676e1485d9
Jonatan Flores <jonatafloress@gmail.com>
2021-12-25 09:07:12 -0300

    make BcryptHashHandler implements HashComparer interface

--- src/infra/gateways/bcrypt-hash-handler.ts
+++ src/infra/gateways/bcrypt-hash-handler.ts
@@ -1,8 +1,8 @@
+import { HashComparer } from '@/domain/gateways'
+
 import bcrypt from 'bcrypt'
 
-jest.mock('bcrypt')
-
-export class BcryptHashHandler {
+export class BcryptHashHandler implements HashComparer {
   async compare (plaintext: string, digest: string): Promise<boolean> {
     return bcrypt.compare(plaintext, digest)
   }
