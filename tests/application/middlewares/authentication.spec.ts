c0b78aacac7c3ffe86af7bf501cdeb6911120101
Jonatan Flores <jonatafloress@gmail.com>
2021-12-26 16:41:00 -0300

    move AuthenticationMiddleware to its own file

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,29 +1,5 @@
-import { forbidden, HttpResponse, ok } from '@/application/helpers'
+import { AuthenticationMiddleware } from '@/application/middlewares'
 import { ForbiddenError } from '@/application/errors'
-import { RequiredStringValidator } from '@/application/validation'
-
-type HttpRequest = { authorization: string }
-type Model = Error | { userId: string }
-type Authorize = (input: { token: string }) => Promise<string>
-
-class AuthenticationMiddleware {
-  constructor (private readonly authorize: Authorize) {}
-
-  async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
-    if (!this.validate({ authorization })) return forbidden()
-    try {
-      const userId = await this.authorize({ token: authorization })
-      return ok({ userId })
-    } catch {
-      return forbidden()
-    }
-  }
-
-  private validate ({ authorization }: HttpRequest): boolean {
-    const error = new RequiredStringValidator(authorization, 'authorization').validate()
-    return error === undefined
-  }
-}
 
 describe('AuthenticationMiddleware', () => {
   let sut: AuthenticationMiddleware
