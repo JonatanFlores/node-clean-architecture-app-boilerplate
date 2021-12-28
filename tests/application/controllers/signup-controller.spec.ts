c7a8bfd290a8be5ed7eb9c03d92ac6bb44556125
Jonatan Flores <jonatafloress@gmail.com>
2021-12-28 07:48:04 -0300

    ensure SignupController returns 200 if AddUserAccount succeeds

--- tests/application/controllers/signup-controller.spec.ts
+++ tests/application/controllers/signup-controller.spec.ts
@@ -1,16 +1,22 @@
+import { Controller } from '@/application/controllers'
 import { RequiredStringValidator, ValidationBuilder as Build, Validator } from '@/application/validation'
+import { HttpResponse, ok } from '@/application/helpers'
 import { AddUserAccount } from '@/domain/usecases'
 
 type HttpRequest = { email: string, password: string }
+type Model = Error | { email: string, accessToken: string }
 
-class SignupController {
-  constructor (private readonly addUserAccount: AddUserAccount) {}
-
-  async handle ({ email, password }: HttpRequest): Promise<void> {
-    await this.addUserAccount({ email, password })
+class SignupController extends Controller {
+  constructor (private readonly addUserAccount: AddUserAccount) {
+    super()
   }
 
-  buildValidators ({ email, password }: HttpRequest): Validator[] {
+  async perform ({ email, password }: HttpRequest): Promise<HttpResponse<Model>> {
+    const result = await this.addUserAccount({ email, password })
+    return ok(result)
+  }
+
+  override buildValidators ({ email, password }: HttpRequest): Validator[] {
     return [
       ...Build.of({ value: email, fieldName: 'email' }).required().build(),
       ...Build.of({ value: password, fieldName: 'password' }).required().build()
@@ -21,13 +27,16 @@ class SignupController {
 describe('SignupController', () => {
   let email: string
   let password: string
+  let accessToken: string
   let addUserAccount: jest.Mock
   let sut: SignupController
 
   beforeAll(() => {
     email = 'any_email@mail.com'
     password = 'any_password'
+    accessToken = 'any_access_token'
     addUserAccount = jest.fn()
+    addUserAccount.mockResolvedValue({ email, accessToken })
   })
 
   beforeEach(() => {
@@ -48,5 +57,14 @@ describe('SignupController', () => {
 
     expect(addUserAccount).toHaveBeenCalledWith({ email, password })
     expect(addUserAccount).toHaveBeenCalledTimes(1)
+  })
+
+  test('should return 200 if AddUserAccount succeeds', async () => {
+    const httpResponse = await sut.handle({ email, password })
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      data: { accessToken, email }
+    })
   })
 })
