7afde910dbcdf9b8c32ee4a145f83a4731698ca7
Jonatan Flores <jonatafloress@gmail.com>
2021-12-28 07:40:27 -0300

    ensure SignupController calls AddUserAccount with correct input

--- tests/application/controllers/signup-controller.spec.ts
+++ tests/application/controllers/signup-controller.spec.ts
@@ -1,8 +1,15 @@
 import { RequiredStringValidator, ValidationBuilder as Build, Validator } from '@/application/validation'
+import { AddUserAccount } from '@/domain/usecases'
 
 type HttpRequest = { email: string, password: string }
 
 class SignupController {
+  constructor (private readonly addUserAccount: AddUserAccount) {}
+
+  async handle ({ email, password }: HttpRequest): Promise<void> {
+    await this.addUserAccount({ email, password })
+  }
+
   buildValidators ({ email, password }: HttpRequest): Validator[] {
     return [
       ...Build.of({ value: email, fieldName: 'email' }).required().build(),
@@ -14,10 +21,17 @@ class SignupController {
 describe('SignupController', () => {
   let email: string
   let password: string
+  let addUserAccount: jest.Mock
   let sut: SignupController
 
+  beforeAll(() => {
+    email = 'any_email@mail.com'
+    password = 'any_password'
+    addUserAccount = jest.fn()
+  })
+
   beforeEach(() => {
-    sut = new SignupController()
+    sut = new SignupController(addUserAccount)
   })
 
   test('should build Validators correctly', () => {
@@ -27,5 +41,12 @@ describe('SignupController', () => {
       new RequiredStringValidator(email, 'email'),
       new RequiredStringValidator(password, 'password')
     ])
+  })
+
+  test('should call AddUserAccount with correct input', async () => {
+    await sut.handle({ email, password })
+
+    expect(addUserAccount).toHaveBeenCalledWith({ email, password })
+    expect(addUserAccount).toHaveBeenCalledTimes(1)
   })
 })
