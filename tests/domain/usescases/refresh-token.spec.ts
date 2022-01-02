3f2cc1ee833a6e5594e04204a5289d6718d17beb
Jonatan Flores <jonatafloress@gmail.com>
2022-01-02 00:05:57 -0300

    ensure RefreshToken calls TokenGenerator with correct input

--- tests/domain/usescases/refresh-token.spec.ts
+++ tests/domain/usescases/refresh-token.spec.ts
@@ -1,37 +1,49 @@
-import { TokenValidator } from '@/domain/contracts/gateways'
+import { TokenValidator, TokenGenerator } from '@/domain/contracts/gateways'
+import { AuthenticationError } from '@/domain/entities/errors'
+import { AccessToken, RefreshToken } from '@/domain/entities'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 
-type Setup = (token: TokenValidator, userRepo: LoadUser) => RefreshToken
-type Input = { refreshToken: string }
-export type RefreshToken = (input: Input) => Promise<void>
+type Setup = (token: TokenValidator & TokenGenerator, userRepo: LoadUser) => RefreshTokenType
+type Input = { currentRefreshToken: string }
+export type RefreshTokenType = (input: Input) => Promise<void>
 
-const setupRefreshToken: Setup = (token, userRepo) => async ({ refreshToken }) => {
-  const userId = await token.validate({ token: refreshToken })
-  await userRepo.load({ id: userId })
+const setupRefreshToken: Setup = (token, userRepo) => async ({ currentRefreshToken }) => {
+  const userId = await token.validate({ token: currentRefreshToken })
+  const user = await userRepo.load({ id: userId })
+  if (user === undefined) throw new AuthenticationError()
+  await token.generate({ key: user.id, expirationInMs: AccessToken.expirationInMs })
+  await token.generate({ key: user.id, expirationInMs: RefreshToken.expirationInMs })
 }
 
 interface LoadUser {
-  load: (input: LoadUser.Input) => Promise<void>
+  load: (input: LoadUser.Input) => Promise<LoadUser.Output>
 }
 
 export namespace LoadUser {
   export type Input = { id: string }
+  export type Output = undefined | {
+    id: string
+    email: string
+  }
 }
 
 describe('RefreshToken', () => {
-  let refreshToken: string
+  let currentRefreshToken: string
   let id: string
-  let token: MockProxy<TokenValidator>
+  let email: string
+  let token: MockProxy<TokenValidator & TokenGenerator>
   let userRepo: MockProxy<LoadUser>
-  let sut: RefreshToken
+  let sut: RefreshTokenType
 
   beforeAll(() => {
-    refreshToken = 'any_refresh_token'
+    currentRefreshToken = 'any_refresh_token'
     id = 'any_user_id'
+    email = 'any_email'
     token = mock()
     token.validate.mockResolvedValue(id)
     userRepo = mock()
+    userRepo.load.mockResolvedValue({ id, email })
   })
 
   beforeEach(() => {
@@ -39,16 +51,27 @@ describe('RefreshToken', () => {
   })
 
   test('should call TokenValidator with correct input', async () => {
-    await sut({ refreshToken })
+    await sut({ currentRefreshToken })
 
-    expect(token.validate).toHaveBeenCalledWith({ token: refreshToken })
+    expect(token.validate).toHaveBeenCalledWith({ token: currentRefreshToken })
     expect(token.validate).toHaveBeenCalledTimes(1)
   })
 
   test('should call TokenValidator with correct input', async () => {
-    await sut({ refreshToken })
+    await sut({ currentRefreshToken })
 
     expect(userRepo.load).toHaveBeenCalledWith({ id })
     expect(userRepo.load).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call TokenGenerator with correct input', async () => {
+    const twoHoursInMs = 2 * 60 * 60 * 1000
+    const thirtyDaysInMs = 30 * 24 * 60 * 60 * 1000
+
+    await sut({ currentRefreshToken })
+
+    expect(token.generate).toHaveBeenCalledWith({ key: id, expirationInMs: twoHoursInMs })
+    expect(token.generate).toHaveBeenCalledWith({ key: id, expirationInMs: thirtyDaysInMs })
+    expect(token.generate).toHaveBeenCalledTimes(2)
   })
 })
