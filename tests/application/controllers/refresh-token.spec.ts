7afdd8412c636ec6b7f2652afa62649d3d7a80c2
Jonatan Flores <jonatafloress@gmail.com>
2022-01-04 12:53:31 -0300

    ensure RefreshTokenController returns 200 with correct data if RefreshToken succeeds

--- tests/application/controllers/refresh-token.spec.ts
+++ tests/application/controllers/refresh-token.spec.ts
@@ -1,13 +1,16 @@
 import { RequiredStringValidator, Validator, ValidationBuilder as Builder } from '@/application/validation'
+import { HttpResponse, ok } from '@/application/helpers'
 import { RefreshTokenType } from '@/domain/usecases'
 
 type HttpRequest = { refreshToken: string }
+type Model = { email: string, accessToken: string, refreshToken: string }
 
 class RefreshTokenController {
   constructor (private readonly refreshToken: RefreshTokenType) {}
 
-  async handle ({ refreshToken }: HttpRequest): Promise<void> {
-    await this.refreshToken({ currentRefreshToken: refreshToken })
+  async handle ({ refreshToken }: HttpRequest): Promise<HttpResponse<Model>> {
+    const result = await this.refreshToken({ currentRefreshToken: refreshToken })
+    return ok(result)
   }
 
   buildValidators ({ refreshToken }: HttpRequest): Validator[] {
@@ -18,13 +21,18 @@ class RefreshTokenController {
 }
 
 describe('RefreshToken', () => {
+  let email: string
+  let accessToken: string
   let refreshToken: string
   let refreshTokenUseCase: jest.Mock
   let sut: RefreshTokenController
 
   beforeAll(() => {
+    email = 'any_email@mail.com'
+    accessToken = 'any_access_token'
     refreshToken = 'any_refresh_token'
     refreshTokenUseCase = jest.fn()
+    refreshTokenUseCase.mockResolvedValue({ email, accessToken, refreshToken })
   })
 
   beforeEach(() => {
@@ -44,5 +52,14 @@ describe('RefreshToken', () => {
 
     expect(refreshTokenUseCase).toHaveBeenCalledWith({ currentRefreshToken: refreshToken })
     expect(refreshTokenUseCase).toHaveBeenCalledTimes(1)
+  })
+
+  test('should return 200 if RefreshToken succeeds', async () => {
+    const httpResponse = await sut.handle({ refreshToken })
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      data: { email, accessToken, refreshToken }
+    })
   })
 })
