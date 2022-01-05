83a5bd3987e970cc416b14197521dfc95582a369
Jonatan Flores <jonatafloress@gmail.com>
2022-01-05 09:31:35 -0300

    ensure LoadLoggedInUser returns an user on success

--- tests/domain/usescases/load-loggedin-user.spec.ts
+++ tests/domain/usescases/load-loggedin-user.spec.ts
@@ -3,20 +3,25 @@ import { mock, MockProxy } from 'jest-mock-extended'
 
 type Setup = (userRepo: LoadUser) => LoadLoggedInUser
 type Input = { id: string }
-export type LoadLoggedInUser = (input: Input) => Promise<void>
+type Output = undefined | { id: string, email: string }
+export type LoadLoggedInUser = (input: Input) => Promise<Output>
 
 const setupLoadLoggedInUser: Setup = (userRepo) => async ({ id }) => {
-  await userRepo.load({ id })
+  const user = await userRepo.load({ id })
+  return user
 }
 
 describe('LoadLoggedInUser', () => {
   let id: string
+  let email: string
   let userRepo: MockProxy<LoadUser>
   let sut: LoadLoggedInUser
 
   beforeAll(() => {
     id = 'any_user_id'
+    email = 'any_user_email'
     userRepo = mock()
+    userRepo.load.mockResolvedValue({ id, email })
   })
 
   beforeEach(() => {
@@ -28,5 +33,11 @@ describe('LoadLoggedInUser', () => {
 
     expect(userRepo.load).toHaveBeenCalledWith({ id })
     expect(userRepo.load).toHaveBeenCalledTimes(1)
+  })
+
+  test('should return an user', async () => {
+    const user = await sut({ id })
+
+    expect(user).toEqual({ id, email })
   })
 })
