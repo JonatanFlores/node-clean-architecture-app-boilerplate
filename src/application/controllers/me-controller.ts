579412ddbd8b3966e5607111884691776ac7948d
Jonatan Flores <jonatafloress@gmail.com>
2022-01-11 10:43:19 -0300

    ensure MeController calls LoadLoggedInUser with correct input

--- src/application/controllers/me-controller.ts
+++ src/application/controllers/me-controller.ts
@@ -1,8 +1,15 @@
 import { ValidationBuilder as Builder, Validator } from '@/application/validation'
+import { LoadLoggedInUser } from '@/domain/usecases'
 
 type HttpRequest = { userId: string }
 
 export class MeController {
+  constructor (private readonly loadLoggedInUser: LoadLoggedInUser) {}
+
+  async handle ({ userId }: HttpRequest): Promise<void> {
+    await this.loadLoggedInUser({ id: userId })
+  }
+
   buildValidators ({ userId }: HttpRequest): Validator[] {
     return [
       ...Builder.of({ value: userId, fieldName: 'userId' }).required().build()
--- tests/application/controllers/me-controller.spec.ts
+++ tests/application/controllers/me-controller.spec.ts
@@ -3,14 +3,16 @@ import { RequiredStringValidator } from '@/application/validation'
 
 describe('MeController', () => {
   let userId: string
+  let loadLoggedInUser: jest.Mock
   let sut: MeController
 
   beforeAll(() => {
+    loadLoggedInUser = jest.fn()
     userId = 'any_user_id'
   })
 
   beforeEach(() => {
-    sut = new MeController()
+    sut = new MeController(loadLoggedInUser)
   })
 
   test('should build Validators correctly', async () => {
@@ -19,5 +21,12 @@ describe('MeController', () => {
     expect(validators).toEqual([
       new RequiredStringValidator(userId, 'userId')
     ])
+  })
+
+  test('should call LoadLoggedInUser with correct input', async () => {
+    await sut.handle({ userId })
+
+    expect(loadLoggedInUser).toHaveBeenCalledWith({ id: userId })
+    expect(loadLoggedInUser).toHaveBeenCalledTimes(1)
   })
 })
