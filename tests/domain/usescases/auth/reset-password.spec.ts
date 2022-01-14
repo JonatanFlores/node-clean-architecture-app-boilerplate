29be96cc841dee3a730977224a7a5eed6bdbee25
Jonatan Flores <jonatafloress@gmail.com>
2022-01-14 11:59:49 -0300

    ensure ResetPassword throws a ResetPasswordTokenError if cannot find the given token

--- tests/domain/usescases/auth/reset-password.spec.ts
+++ tests/domain/usescases/auth/reset-password.spec.ts
@@ -1,41 +1,63 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 
-type Setup = (userToken: LoadUserToken) => ResetPassword
+type Setup = (userTokenRepo: LoadUserToken) => ResetPassword
 type Input = { token: string, password: string }
 export type ResetPassword = (input: Input) => Promise<void>
 
-const setupResetPassword: Setup = (userToken) => async ({ token }) => {
-  await userToken.load({ token })
+const setupResetPassword: Setup = (userTokenRepo) => async ({ token }) => {
+  const userToken = await userTokenRepo.load({ token })
+  if (userToken === undefined) throw new ResetPasswordTokenError()
 }
 
 export interface LoadUserToken {
-  load: (input: LoadUserToken.Input) => Promise<void>
+  load: (input: LoadUserToken.Input) => Promise<LoadUserToken.Output>
 }
 
 export namespace LoadUserToken {
   export type Input = { token: string }
+  export type Output = undefined | { id: string, userId: string, token: string }
+}
+
+export class ResetPasswordTokenError extends Error {
+  constructor () {
+    super('Invalid reset password token')
+    this.name = 'ResetPasswordTokenError'
+  }
 }
 
 describe('ResetPassword', () => {
   let password: string
   let token: string
-  let userToken: MockProxy<LoadUserToken>
+  let userTokenRepo: MockProxy<LoadUserToken>
   let sut: ResetPassword
 
   beforeAll(() => {
     password = 'any_password'
     token = 'any_reset_password_token'
-    userToken = mock()
+    userTokenRepo = mock()
+    userTokenRepo.load.mockResolvedValue({
+      id: 'any_user_token_id',
+      userId: 'any_user_id',
+      token
+    })
   })
 
   beforeEach(() => {
-    sut = setupResetPassword(userToken)
+    sut = setupResetPassword(userTokenRepo)
   })
 
   test('should call LoadUserToken with correct input', async () => {
     await sut({ token, password })
 
-    expect(userToken.load).toHaveBeenCalledWith({ token })
-    expect(userToken.load).toHaveBeenCalledTimes(1)
+    expect(userTokenRepo.load).toHaveBeenCalledWith({ token })
+    expect(userTokenRepo.load).toHaveBeenCalledTimes(1)
+  })
+
+  test('should throw ResetPasswordTokenError if cannot find the given token', async () => {
+    userTokenRepo.load.mockResolvedValueOnce(undefined)
+
+    const promise = sut({ token, password })
+
+    await expect(promise).rejects.toThrow(new ResetPasswordTokenError())
   })
 })
