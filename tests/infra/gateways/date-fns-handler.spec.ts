951a4d5a9723eed51d9e10d2e2a7ac368f2e0124
Jonatan Flores <jonatafloress@gmail.com>
2022-01-14 23:18:39 -0300

    ensure DateFnsHandler calls differenceInHours with correct arguments

---
+++ tests/infra/gateways/date-fns-handler.spec.ts
@@ -0,0 +1,45 @@
+import * as datefns from 'date-fns'
+import MockDate from 'mockdate'
+
+jest.mock('date-fns')
+
+class DateFnsHandler {
+  diffInHours (dateLeft: number | Date, dateRight: number | Date): void {
+    datefns.differenceInHours(dateLeft, dateRight)
+  }
+}
+
+describe('DateFnsHandler', () => {
+  let dateRight: Date
+  let dateLeft: Date
+  let sut: DateFnsHandler
+  let fakeDateFns: jest.Mocked<typeof datefns>
+
+  beforeAll(() => {
+    MockDate.set(new Date())
+    dateLeft = new Date()
+    dateRight = new Date()
+    fakeDateFns = datefns as jest.Mocked<typeof datefns>
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  beforeEach(() => {
+    sut = new DateFnsHandler()
+  })
+
+  describe('diffInHours', () => {
+    beforeAll(() => {
+      fakeDateFns.differenceInHours.mockImplementation(() => 2)
+    })
+
+    test('should call differenceInHours with correct input', async () => {
+      sut.diffInHours(dateLeft, dateRight)
+
+      expect(fakeDateFns.differenceInHours).toHaveBeenCalledWith(dateLeft, dateRight)
+      expect(fakeDateFns.differenceInHours).toHaveBeenCalledTimes(1)
+    })
+  })
+})
