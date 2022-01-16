eb85e40d5560f426355d4a9a589048f2713da537
Jonatan Flores <jonatafloress@gmail.com>
2022-01-16 17:00:42 -0300

    removed test code from production code

--- src/infra/gateways/date-fns-handler.ts
+++ src/infra/gateways/date-fns-handler.ts
@@ -2,8 +2,6 @@ import { DateDifferenceInHours } from '@/domain/contracts/gateways'
 
 import { differenceInHours } from 'date-fns'
 
-jest.mock('date-fns')
-
 export class DateFnsHandler implements DateDifferenceInHours {
   diffInHours (dateLeft: number | Date, dateRight: number | Date): number {
     return differenceInHours(dateLeft, dateRight)
