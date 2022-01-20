79ac69f3f8f297a5b7b3879e2a9ccb3b5910fd18
Jonatan Flores <jonatafloress@gmail.com>
2022-01-20 00:28:11 -0300

    ensure ConfirmUserAccountCreation calls DateDifferenceInHours with correct input

--- tests/domain/usescases/auth/confirm-user-account-creation.spec.ts
+++ tests/domain/usescases/auth/confirm-user-account-creation.spec.ts
@@ -1,9 +1,10 @@
+import { DateDifferenceInHours } from '@/domain/contracts/gateways'
 import { LoadUserToken } from '@/domain/contracts/repos/mongo'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 import MockDate from 'mockdate'
 
-type Setup = (userTokenRepo: LoadUserToken) => ConfirmUserAccountCreation
+type Setup = (userTokenRepo: LoadUserToken, dateAdapter: DateDifferenceInHours) => ConfirmUserAccountCreation
 type Input = { token: string }
 export type ConfirmUserAccountCreation = (input: Input) => Promise<void>
 
@@ -14,26 +15,33 @@ export class ConfirmUserAccountTokenNotFoundError extends Error {
   }
 }
 
-export const setupConfirmUserAccountCreation: Setup = (userTokenRepo) => async ({ token }) => {
+export const setupConfirmUserAccountCreation: Setup = (userTokenRepo, dateAdapter) => async ({ token }) => {
   const userToken = await userTokenRepo.load({ token })
   if (userToken === undefined) throw new ConfirmUserAccountTokenNotFoundError()
+  const { createdAt } = userToken
+  dateAdapter.diffInHours(new Date(createdAt), new Date())
 }
 
 describe('ConfirmUserAccountCreation', () => {
   let token: string
+  let createdAt: string
   let userTokenRepo: MockProxy<LoadUserToken>
+  let dateAdapter: MockProxy<DateDifferenceInHours>
   let sut: ConfirmUserAccountCreation
 
   beforeAll(() => {
     MockDate.set(new Date())
     token = 'any_token'
+    createdAt = (new Date(2022, 0, 1, 0)).toISOString()
     userTokenRepo = mock()
     userTokenRepo.load.mockResolvedValue({
       id: 'any_user_token_id',
       userId: 'any_user_id',
-      createdAt: (new Date(2022, 0, 1, 0)).toISOString(),
+      createdAt,
       token
     })
+    dateAdapter = mock()
+    dateAdapter.diffInHours.mockReturnValue(1)
   })
 
   afterAll(() => {
@@ -41,7 +49,7 @@ describe('ConfirmUserAccountCreation', () => {
   })
 
   beforeEach(() => {
-    sut = setupConfirmUserAccountCreation(userTokenRepo)
+    sut = setupConfirmUserAccountCreation(userTokenRepo, dateAdapter)
   })
 
   test('should call LoadUserToken with correct input', async () => {
@@ -57,5 +65,20 @@ describe('ConfirmUserAccountCreation', () => {
     const promise = sut({ token })
 
     await expect(promise).rejects.toThrow(new ConfirmUserAccountTokenNotFoundError())
+  })
+
+  test('should call DateDifferenceInHours with correct input', async () => {
+    const previousDate = new Date()
+    const threeHoursAfterCreatedAt = new Date(2022, 0, 3, 0)
+    MockDate.set(threeHoursAfterCreatedAt)
+
+    await sut({ token })
+
+    expect(dateAdapter.diffInHours).toHaveBeenCalledTimes(1)
+    expect(dateAdapter.diffInHours).toHaveBeenCalledWith(
+      new Date(createdAt),
+      threeHoursAfterCreatedAt
+    )
+    MockDate.set(previousDate)
   })
 })
