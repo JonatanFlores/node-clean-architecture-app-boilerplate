842a8d5a4e51c87377beb9bcd4b576815e05d422
Jonatan Flores <jonatafloress@gmail.com>
2023-01-10 09:30:02 -0300

    change routes

--- src/main/routes/ping-routes.ts
+++ src/main/routes/ping-routes.ts
@@ -4,6 +4,6 @@ import { makePingController } from '@/main/factories/application/controller'
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.get('/', (_, response) => response.json({ message: 'Welcome to our api' }))
-  router.get('/ping', adapt(makePingController()))
+  router.get('/api', (_, response) => response.json({ message: 'Welcome to our api' }))
+  router.get('/api/ping', adapt(makePingController()))
 }
