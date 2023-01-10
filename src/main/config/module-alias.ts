2948690aec21dd78ea70015cbbbf6068101b88b6
Jonatan Flores <jonatafloress@gmail.com>
2023-01-10 09:31:07 -0300

    change module-alias

--- src/main/config/module-alias.ts
+++ src/main/config/module-alias.ts
@@ -1,5 +1,4 @@
 import { addAlias } from 'module-alias'
 import { resolve } from 'path'
 
-// addAlias('@', resolve(process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'))
-addAlias('@', resolve('src'))
+addAlias('@', resolve(process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'))
